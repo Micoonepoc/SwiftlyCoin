f473d1f9631702e48ca2470f9436b97fd2575354
Mykhailo <118993099+Micoonepoc@users.noreply.github.com>
2023-11-28 19:35:00 +0100

    Project added to github

---
+++ SwiftlyCoin/SwiftlyCoin/SwiftlyCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftlyCoinApp.swift
+//  SwiftlyCoin
+//
+//  Created by Михаил on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftlyCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
